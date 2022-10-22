fcf03c2f66f6b382a7cadf9b3f0144085bd99d18
wogus4048 <wogus4048@naver.com>
2022-10-22 17:01:57 +0900

    무중단배포 실험

--- src/main/java/com/example/demo/src/test/TestController.java
+++ src/main/java/com/example/demo/src/test/TestController.java
@@ -40,7 +40,7 @@ public class TestController {
         logger.error("ERROR Level 테스트");
 
 
-        return "Success Test version -> 0.0.2";
+        return "Success Test version -> 0.0.3";
     }
     //planet 저장소 , ori branch push 실험입니다.
     //git add 테스트
